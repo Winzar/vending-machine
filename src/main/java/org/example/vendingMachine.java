f251c65508b3c3ceee6a66b4a9661da0f5d93a76
Winzar <connorwinzar@gmail.com>
2022-05-25 12:40:17 +0100

    Repo initilisation

---
+++ src/main/java/org/example/vendingMachine.java
@@ -0,0 +1,34 @@
+//package org.example;
+//
+//import java.util.Scanner;
+//
+//public class vendingMachine {
+//
+//    public static void Test(String[] args){
+//    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+//    System.out.println("Enter username");
+//
+//
+//    String userName = myObj.nextLine();  // Read user input
+//    System.out.println("Username is: " + userName);  // Output user input
+//    }
+//
+////    public class menu(String[] ) {
+////        String Statement1= "1) Buy";
+////        String Statement2= "2) Refill Item";
+////        String Statement3= "3) See What Is Available";
+////        String Statement4= "4) Stock Check";
+////
+//////        System.out.println("1) Buy");
+//////        System.out.println("1) Buy");
+//////        System.out.println("2) Refill Item");
+//////        System.out.println("3) See What Is Available");
+//////        System.out.println("4) Stock Check");
+////    }
+//
+//
+//    public static void main(String[] args){
+//        vendingMachine test = new vendingMachine();
+//        System.out.println(test.getClass());
+//    }
+//}
